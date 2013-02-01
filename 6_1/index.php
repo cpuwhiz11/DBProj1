2e5f00ec76b909d32e07b8e295bc92f755dcf764
paul <paul@192.168.1.110>
2013-01-31 22:30:30 -0500

    Init Commit

---
+++ 6_1/index.php
@@ -0,0 +1,52 @@
+<?php
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/include/classes/Database.class.php");
+    
+    $db = new Database("db453524503");
+    $db->connect();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <form action="index.php" method="post">
+            
+            <select name="studio">
+                <option value="">Select a studio...</option>
+                
+                <?php
+                
+                    $db->query("SELECT DISTINCT name FROM Studio");
+                    
+                    $studios = $db->getAssocResult();
+                    
+                    foreach($studios as $studio) {
+                        
+                        echo "<option value='" . $studio["name"] . "'>" . $studio["name"] . "</option>";
+                    
+                    }
+                
+                ?>
+                
+            </select>
+            
+        </form>
+        
+        <?php
+            
+            
+            
+            
+            
+            $db->query("SELECT * FROM Movies");
+            
+            echo "<pre>";
+            print_r($db->getAssocResult());
+            echo "</pre>";
+            
+        ?>
+    </body>
+</html>
