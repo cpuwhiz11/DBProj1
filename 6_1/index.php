24cddc1c23457c6815a53cdb2a0a0ce693900793
Ryan <Ryan@192.168.3.102>
2013-02-07 11:11:28 -0500

    Completed file for query 6.1

--- 6_1/index.php
+++ 6_1/index.php
@@ -8,97 +8,114 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+		<link href="/include/css/styles.css" rel="stylesheet" type="text/css"/>
+        <title>6.1</title>
     </head>
     <body>
         
-        <form action="index.php" method="post">
-            
-            <select name="studio">
-                <option value="">Select a studio...</option>
-                
-                <?php
-                 
-                    $db->query("SELECT DISTINCT name FROM Studio");
-                    
-                    $studios = $db->getAssocResult();
-                    
-                    foreach($studios as $studio) {
-                        
-                        echo "<option value='" . $studio["name"] . "'>" . $studio["name"] . "</option>\n";
-                    
-                    }
-                
-                ?>
-                
-            </select>
-			
-			<select name="year">
-				<option value="">Select a year...</option>
-				<?php
+		<div class="content">
+			<div class="searchForm">
+				<p>Select a movie studio and year to find the relevant movies.</p>
+				<form action="index.php" method="post">
+
+					<select name="studio">
+						<option value="">Select a studio...</option>
+
+						<?php
+
+							$db->query("SELECT DISTINCT name FROM Studio");
+
+							$studios = $db->getAssocResult();
+
+							foreach($studios as $studio) {
+
+								echo "<option value='" . $studio["name"] . "'>" . $studio["name"] . "</option>\n";
+
+							}
+
+						?>
+
+					</select>
+
+					<select name="year">
+						<option value="">Select a year...</option>
+						<?php
+
+							$db->query("SELECT DISTINCT year FROM Movies ORDER BY year ASC");
+
+							$years = $db->getAssocResult();
+
+							foreach($years as $year) {
+
+								echo "<option value='" . $year["year"] . "'>" . $year["year"] . "</option>\n";
+
+							}
+
+						?>				
+					</select>
+
+					<input type="submit" value="Search"/>
+
+				</form>
+			</div>
+
+			<?php
+
+				$selectedStudio = $_POST["studio"];
+				$selectedYear	= $_POST["year"];
 				
-					$db->query("SELECT DISTINCT year FROM Movies ORDER BY year ASC");
-					
-					$years = $db->getAssocResult();
-					
-					foreach($years as $year) {
-						
-						echo "<option value='" . $year["year"] . "'>" . $year["year"] . "</option>\n";
-						
+				if(strlen($selectedStudio) > 0 && strlen($selectedYear) > 0) {
+					$db->query("SELECT * FROM Movies WHERE studioName='" . $selectedStudio . "' AND year=" . $selectedYear);
+
+					$searchResult = $db->getAssocResult();
+
+					if(count($searchResult) > 0) {
+
+						echo "<table class='resultTable'>";
+						echo "<thead>";
+						echo "<tr>";
+						echO "<td>Title</td>";
+						echo "<td>Year</td>";
+						echo "<td>Length</td>";
+						echo "<td>Genre</td>";
+						echo "<td>Studio Name</td>";
+						echo "<td>Producer C#</td>";
+						echo "</tr>";
+						echo "</thead>";
+						echo "<tbody>";
+
+						foreach($searchResult as $result) {
+
+							echo "<tr>";
+
+							foreach($result as $r) {
+
+								echo "<td>" . $r . "</td>";
+
+							}
+
+							echo "</tr>";
+
+
+						}
+
+						echo "</tbody>";
+						echo "</table>";
+
 					}
 					
-				?>				
-			</select>
-			
-			<input type="submit" value="Search"/>
-            
-        </form>
-        
-        <?php
-            			
-			$selectedStudio = $_POST["studio"];
-			$selectedYear	= $_POST["year"];
-			
-            $db->query("SELECT * FROM Movies WHERE studioName='" . $selectedStudio . "' AND year=" . $selectedYear);
-            
-			$searchResult = $db->getAssocResult();
-			
-			
-			echo "<table>";
-			echo "<thead>";
-			echo "<tr>";
-			echO "<td>Title</td>";
-			echo "<td>Year</td>";
-			echo "<td>Length</td>";
-			echo "<td>Genre</td>";
-			echo "<td>Studio Name</td>";
-			echo "<td>Producer C#</td>";
-			echo "</tr>";
-			echo "</thead>";
-			echo "<tbody>";
-
-			foreach($searchResult as $result) {
-				
-				echo "<tr>";
-				
-				foreach($result as $r) {
+				}
+				else {
 					
-					echo "<td>" . $r . "</td>";
+					echo "<p>No results found!</td>"; 
 					
 				}
-				
-				echo "</tr>";
-				
-		
-			}
-			
-			echo "</tbody>";
-			echo "</table>";
-			
-            echo "<pre>";
-            print_r($searchResult);
-            echo "</pre>";
-            
-        ?>
+
+				echo "<pre>";
+				print_r($searchResult);
+				echo "</pre>";
+
+			?>
+		</div>
     </body>
 </html>
