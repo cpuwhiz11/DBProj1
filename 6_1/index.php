53cd7b943c19206b6926eaf365eb5dce3c9d0c3b
Ryan <Ryan@192.168.3.102>
2013-02-07 10:46:59 -0500

    Added label to studio drop down

--- 6_1/index.php
+++ 6_1/index.php
@@ -6,7 +6,7 @@
 ?>
 <!DOCTYPE html>
 <html>
-    <head> 
+    <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
     </head>
@@ -18,33 +18,85 @@
                 <option value="">Select a studio...</option>
                 
                 <?php
-                
+                 
                     $db->query("SELECT DISTINCT name FROM Studio");
                     
                     $studios = $db->getAssocResult();
                     
                     foreach($studios as $studio) {
                         
-                        echo "<option value='" . $studio["name"] . "'>" . $studio["name"] . "</option>";
+                        echo "<option value='" . $studio["name"] . "'>" . $studio["name"] . "</option>\n";
                     
                     }
                 
                 ?>
                 
             </select>
+			
+			<select name="year">
+				<option value="">Select a year...</option>
+				<?php
+				
+					$db->query("SELECT DISTINCT year FROM Movies ORDER BY year ASC");
+					
+					$years = $db->getAssocResult();
+					
+					foreach($years as $year) {
+						
+						echo "<option value='" . $year["year"] . "'>" . $year["year"] . "</option>\n";
+						
+					}
+					
+				?>				
+			</select>
+			
+			<input type="submit" value="Search"/>
             
         </form>
         
         <?php
+            			
+			$selectedStudio = $_POST["studio"];
+			$selectedYear	= $_POST["year"];
+			
+            $db->query("SELECT * FROM Movies WHERE studioName='" . $selectedStudio . "' AND year=" . $selectedYear);
             
-            
-            
-            
-            
-            $db->query("SELECT * FROM Movies");
-            
+			$searchResult = $db->getAssocResult();
+			
+			
+			echo "<table>";
+			echo "<thead>";
+			echo "<tr>";
+			echO "<td>Title</td>";
+			echo "<td>Year</td>";
+			echo "<td>Length</td>";
+			echo "<td>Genre</td>";
+			echo "<td>Studio Name</td>";
+			echo "<td>Producer C#</td>";
+			echo "</tr>";
+			echo "</thead>";
+			echo "<tbody>";
+
+			foreach($searchResult as $result) {
+				
+				echo "<tr>";
+				
+				foreach($result as $r) {
+					
+					echo "<td>" . $r . "</td>";
+					
+				}
+				
+				echo "</tr>";
+				
+		
+			}
+			
+			echo "</tbody>";
+			echo "</table>";
+			
             echo "<pre>";
-            print_r($db->getAssocResult());
+            print_r($searchResult);
             echo "</pre>";
             
         ?>
