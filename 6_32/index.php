777fc73d8676fa638be86fd86c53123b0651852b
Paul Geromini <pgeromini@crossbeam.com>
2013-02-14 10:52:20 -0500

    Finish 6.21, 6.32, 6.34

--- 6_32/index.php
+++ 6_32/index.php
@@ -1,11 +1,77 @@
+<?php
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/include/classes/Database.class.php");
+    
+    $db = new Database("db453524503");
+    $db->connect();
+?>
+<!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+		<link href="/include/css/styles.css" rel="stylesheet" type="text/css"/>
+        <title>6.1</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
+		<div class="content">
+			<div class="navigation">
+				<ul>
+					<li><a href="/6_1/">6.1</a></li>
+					<li><a href="/6_2/">6.2</a></li>
+					<li><a href="/6_11/">6.11</a></li>
+					<li><a href="/6_12/">6.12</a></li>
+					<li><a href="/6_14/">6.14</a></li>
+					<li><a href="/6_20/">6.20</a></li>
+					<li><a href="/6_21/">6.21</a></li>
+					<li><a href="/6_31/">6.31</a></li>
+					<li><a href="/6_32/">6.32</a></li>
+					<li><a href="/6_34/">6.34</a></li>
+				</ul>				
+			</div>
+			<div class="searchForm">
+				<p>Length of films produced by each producer.</p>
+			</div>
+
+			<?php
+                                    $db->query("SELECT MovieExec.name, SUM(Movies.length) as Length
+                                                FROM Movies
+                                                INNER JOIN MovieExec 
+                                                ON Movies.`producerC#` = MovieExec.`cert#`
+                                                GROUP BY MovieExec.name");
+
+                                    $searchResult = $db->getAssocResult();
+
+                                    if(count($searchResult) > 0) {
+
+                                            echo "<table class='resultTable'>";
+                                            echo "<thead>";
+                                            echo "<tr>";
+                                            echo "<td>Name</td>";
+                                            echo "<td>Length</td>";
+                                            echo "</tr>";
+                                            echo "</thead>";
+					    echo "<tbody>";
+                                        
+                                            foreach($searchResult as $result) {
+
+                                                    echo "<tr>";
+                                                    echo "<td>" . $result["name"] . "</td>";
+                                                    echo "<td>" . $result["Length"] . "</td>";
+                                                    echo "</tr>";
+
+                                            }
+
+                                            echo "</tbody>";
+                                            echo "</table>";
+
+                                    }
+                                    else {
+
+                                            echo "<p>No results found!</p>"; 
+
+                                    }
+
+			?>
+		</div>
     </body>
 </html>
